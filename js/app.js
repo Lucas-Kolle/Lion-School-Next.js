fed9e81f4ac0594cfa4d7e6532fa29670030a510
Lucas Kolle <lucaskolle2020@gmail.com>
2026-08-04 09:23:40 -0300

    excluindo tudo

--- js/app.js
+++
@@ -1,7 +0,0 @@
-import { calcularSoma } from "./calculos.js"
-
-const calcular = document.getElementById("calcular")
-
-const numeros = [1,2,3,4,5,6,7,8]
-
-calcular.addEventListener("click", () => calcularSoma(numeros))
--- js/calculos.js
+++
@@ -1,4 +0,0 @@
-export const calcularSoma = function(numeros){
-    
-}
-
