df442f18ba3de93ea22fe8d665b34a7b5cdf213e
LUCAS DIAS BRANDAO KOLLE <25203704@CFP127ALUNO.LOCAL>
2026-08-12 09:56:42 -0300

    mudando algumas coisas

--- src/components/footers/footer_principal.js
+++ src/components/footers/footer_principal.js
@@ -2,7 +2,7 @@
 export default function Footer() { /* O export serve para permitir que esse componente seja usado em outros arquivos.
     O default significa que este é o componente principal desse arquivo. */
 return (
-    <footer className="bg-gray-800 text-white p-4">
+    <footer className="h-[15vh] bg-[#3347B0]">
         <text>Copyright © 2026 | Fernando Leonid</text>
     </footer>
 )
--- src/components/headers/header.js
+++ src/components/headers/header.js
@@ -1,9 +1,19 @@
+import Image from "next/image" 
+
+
 //Criando um componete chamado Header. Um componente é uma função que retorna um elemento HTML.
 export default function Header() { /* O export serve para permitir que esse componente seja usado em outros arquivos.
                                         O default significa que este é o componente principal desse arquivo. */
     return (
-        <header className=" h-[10vh] bg-[#3347B0]">
-            <h1>Testando Header</h1>
+        <header className=" h-[15vh] bg-[#3347B0]">
+            <div>
+                <Image 
+                    src="/"
+                    alt="Logo do site"
+                    width={300}
+                    height={100}
+                />
+            </div>
         </header>
     )
 }
