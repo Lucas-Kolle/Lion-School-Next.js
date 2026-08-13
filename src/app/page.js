4e52c5d8613ede27a22e7ae340f3df1ce68f065f
Lucas Kolle <lucaskolle2020@gmail.com>
2026-08-13 10:32:01 -0300

    finalizando por hoje

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,8 @@ import Link from "next/link"; // Importando o componente Link do Next.js para na
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold">Página Home</h1>
+      <text>Escolha um <span>curso</span> <br/>
+        para gerenciar</text>
     </div>
   );
 }
